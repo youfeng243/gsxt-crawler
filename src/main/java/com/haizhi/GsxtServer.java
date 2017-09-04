a3df41fc5d597d82daa1b436d9d492a0e75660a6
youfeng <youfeng@haizhi.com>
2017-09-04 12:12:57 +0800

    基本示例跑通

--- src/main/java/com/haizhi/GsxtServer.java
+++ src/main/java/com/haizhi/GsxtServer.java
@@ -1,6 +1,10 @@
 package com.haizhi;
 
 import com.haizhi.util.PropertyUtil;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCompute;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.lang.IgniteRunnable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,5 +23,26 @@ public class GsxtServer {
 
     public static void main(String... args) {
         logger.info("启动工商抓取程序...");
+
+        try (Ignite ignite = Ignition.start("example-ignite.xml")) {
+            System.out.println();
+            System.out.println("Compute runnable example started.");
+
+            IgniteCompute compute = ignite.compute();
+
+            // Iterate through all words in the sentence and create runnable jobs.
+            for (final String word : "Print words using runnable".split(" ")) {
+                // Execute runnable on some node.
+                compute.run((IgniteRunnable) () -> {
+                    System.out.println();
+                    System.out.println(">>> Printing '" + word + "' on this node from ignite job.");
+                });
+            }
+
+            System.out.println();
+            System.out.println(">>> Finished printing words using runnable execution.");
+            System.out.println(">>> Check all nodes for output (this node is also part of the cluster).");
+        }
+
     }
 }
